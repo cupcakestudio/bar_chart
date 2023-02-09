0d92e5f6c49461df0c3ccf171ac277f0b8f3f3d9
Ting Hua Lin <61328447+cupcakestudio@users.noreply.github.com>
2023-02-09 18:10:59 +0100

    Update script.js
    bars get and display correct rand value
    color styling

--- script.js
+++ script.js
@@ -1,62 +1,66 @@
 "use strict";
 let barChartContainer = document.querySelector("#bar_chart_animation");
 
-let barDesign; //styled bar
-const barChart = [
-  1, 10, 17, 27, 19, 32, 11, 24, 16, 33, 8, 29, 40, 30, 37, 9, 15, 18, 7, 14,
-  23, 39, 20, 22, 36, 5, 25, 38, 12, 26, 31, 6, 3, 34, 13, 28, 4, 35, 2, 21,
-];
+const barChart = [];
 
-//'counter'
+window.addEventListener("DOMContentLoaded", initLoop())
+
 function initLoop() {
-  arrayOFCustomersLoop();
-  const j = queueSize;
-}
-// showBar();
+  arrayOfCustomersLoop();
 
-// view
+}
+
+// VIEW
 function showBar() {
-  for (let i = 0; i < 20; i++) {
+  //clear all content so that the new bars with old measured bars can show, so you replace the whole array once.
+  //and not getting a new array shown each time the loop runs
+   barChartContainer.innerHTML = "";
+  for (let i = 0; i < 40; i++) {0
     const barMeasurement = document.createElement("div");
     barMeasurement.classList.add("bar_measurement");
     barChartContainer.appendChild(barMeasurement);
-    // const queueSize = getNumberOfCustomers();
-    // get index value as new queueSize
-    barMeasurement.style.height = `${i * 2}px`;
+  
+    // get index value as new queueSize, can just call barChart[i], cause of global scope
+    barMeasurement.style.height = `${barChart[i]}px`;
     barMeasurement.style.width = `45px`;
-    setTimeout(1000);
-    // barMeasurement.style.transform = `translate(45px)`;
+
+    //color styling
+    if (`${barChart[i]}` <= 10){
+    barMeasurement.style.backgroundColor = "red";}
+    else if (`${barChart[i]}` > 10 && `${barChart[i]}` <= 25){
+      barMeasurement.style.backgroundColor = "blue";}
+   
   }
-  // // barDesign = barMeasurement.;
-  // barChartContainer.innerHTML +=
-  //   "<div </div>";
-  //   barChart
-  // barChartContainer.appendChild(barMeasurement);
+ 
 }
-showBar();
-//Model, alt data -
-function arrayOFCustomersLoop() {
-  // with fixed array value
-  let i = 0;
-  const queueSize = barChart[i];
-  i++;
+
+//MODEL, alt data -
+function arrayOfCustomersLoop() {
+
+  //with random values
+  const queueSize = getNumberOfCustomers();
+
+if (barChart.length < 40) {
+  //if array is not filled with 40 items, add to end of array
   barChart.push(queueSize);
-  barChart.shift();
 
   console.log(barChart);
-  // setTimeout(arrayOFCustomersLoop, 2000);
-  // showBar(barChart);
-  //with random values
-  // const queueSize = getNumberOfCustomers();
-
-  // barChart.shift();
-  // barChart.push(queueSize);
-
-  // console.log(barChart);
-  // setTimeout(arrayOFCustomersLoop, 2000);
+   showBar();
+  
+   setTimeout(arrayOfCustomersLoop, 1000);
+}
+ else {
+  // if filled, push new queueSize value to end of array and shift everything one to the left. so that you shift out first item.
+  barChart.shift();
+   barChart.push(queueSize);
+ 
+  console.log(barChart);
+  showBar()
+   setTimeout(arrayOfCustomersLoop, 1000);
+}
 }
 
-//Controller
+//CONTROLLER
 //function from brief, to get number of Customers, acts as our queue data
 function getNumberOfCustomers() {
   //FAKE data, gives a compeletely random int between 0 to 32
